0e013d60aa97d2e7d03fb1c1e0d327883565bf30
nave17 <yousifbinyehia@gmail.com>
2023-05-03 07:15:19 +0200

    Incomplete App Missing Features: Incomplete Gui for PartCheck, InventoryCheck and EmployeesList Incomplete Code for PartCheck, InventoryCheck and EmployeesList Incomplete Application Start Intro Incomplete design, Add round buttons instead of the old ones Other Features may be missing and not mentioned but these are the main ones

---
+++ src/StandardClasses/User.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package StandardClasses;
+
+/**
+ *
+ * @author yousi
+ */
+public class User {
+    
+    private int AL;
+    private int ID;
+    private String Title;
+    private String name;
+    public User(String name, int ID, String Title, int AL){
+    this.name=name;
+    this.ID=ID;
+    this.Title=Title;
+    this.AL=AL;
+    }
+}
